5498005be6f6e7793cb1a99febccaa0d4532c55d
houzi- <wojiaolinmu008@gmail.com>
2018-04-09 23:22:58 +0800

    Update version

--- config.json.js
+++ config.json.js
@@ -1,9 +1,9 @@
 {
     "build_date": "2018-04-09_14:02:15", 
-    "description": "帮你自动签到", 
+    "description": "这是一只自动帮你签到的狗", 
     "home_url": "Module_qiandao.asp", 
-    "md5": "721c8b4a91f945faea18e6fde24c67c9", 
+    "md5": "25b09c0f379a5763e853a12470de7884", 
     "module": "qiandao", 
     "title": "自动签到", 
-    "version": "1.1"
+    "version": "1.2"
 }
