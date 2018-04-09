12e5fa520eb96a3e2ce93fe7351597134a167dca
sadoneli <sadoneli@gmail.com>
2018-04-09 23:43:52 +0800

    update logo

--- config.json.js
+++ config.json.js
@@ -1,9 +1,9 @@
 {
     "build_date": "2018-04-09_14:02:15", 
-    "description": "这是一只自动帮你签到的狗", 
+    "description": "这是一只自动帮你签到的dog", 
     "home_url": "Module_qiandao.asp", 
-    "md5": "25b09c0f379a5763e853a12470de7884", 
+    "md5": "d361f90b86578219f3a436b390722e5b", 
     "module": "qiandao", 
-    "title": "自动签到", 
-    "version": "1.2"
+    "title": "签到dog", 
+    "version": "1.3"
 }
